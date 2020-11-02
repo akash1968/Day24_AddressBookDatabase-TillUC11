9221aa9d2e7c2515d605c068ed6719e24e68bc0b
Akash <akash.singh670@gmail.com>
2020-11-02 08:18:34 +0530

    [Akash]. Added UC7GetSizeOfAddressBookByCityOrState

---
+++ UC7GetSizeOfAddressBookByCityOrState.sql
@@ -0,0 +1,14 @@
+--UC 7 : Ability to understand the size of address book by City and State
+
+use addressbook_service;
+--Inserting more data into table
+insert into addressbook values
+('Peter','Parker','Sector 32','Surat','Gujrat',137667,7765434567,'peter@gmail.com'),
+('Virat','Kohli','Block 2','Varodra','Gujrat',138675,9876545678,'virat@gmail.com');
+select * from addressbook;
+--Find size of address book by city
+select City,count(City) as PeopleInCity from addressbook group by City;
+--Find size of address book by State
+select State_Name,count(State_Name) as PeopleInState from addressbook group by State_Name;
+
+
